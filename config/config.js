0bbeb487af4034cf821a382ce77a761c4a8a9f0c
Anrin Deo Avis <anrin11@gmail.com>
2015-05-22 11:48:24 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,13 +1,10 @@
 // The server port - the port to run Pokemon Showdown under
 exports.port = 8000;
 
-// The server ID - a unique ID describing this Showdown server
-exports.serverid = 'y';
-
-// The server token - to access the login database and ladder on pokemonshowdown.com
-//   This token must be registered for accessing the ladder, but you will
-//   still be able to login with an unregistered token.
-exports.servertoken = '';
+// The server id - the id specified in the server registration.
+//   This should be set properly especially when there are more than one
+//   pokemon showdown server running from the same IP
+exports.serverId = 'killthenoise';
 
 // proxyIps - proxy IPs with trusted X-Forwarded-For headers
 //   This can be either false (meaning not to trust any proxies) or an array
@@ -42,16 +39,14 @@ exports.loginServer = {
 };
 
 // crashGuardEmail - if the server has been running for more than an hour
-// and crashes, send an email using these settings, rather than locking down
-// the server. Uncomment this definition if you want to use this feature;
-// otherwise, all crashes will lock down the server.
+//   and crashes, send an email using these settings, rather than locking down
+//   the server. Uncomment this definition if you want to use this feature;
+//   otherwise, all crashes will lock down the server.
 /**exports.crashGuardEmail = {
-	transport: 'SMTP',
 	options: {
 		host: 'mail.example.com',
 		port: 465,
-		secureConnection: true,
-		maxConnections: 1,
+		secure: true,
 		auth: {
 			user: 'example@domain.com',
 			pass: 'password'
@@ -65,14 +60,27 @@ exports.loginServer = {
 // report joins and leaves - shows messages like "<USERNAME> joined"
 //   Join and leave messages are small and consolidated, so there will never
 //   be more than one line of messages.
+//   If this setting is set to `true`, it will override the client-side
+//   /hidejoins configuration for users.
 //   This feature can lag larger servers - turn this off if your server is
 //   getting more than 80 or so users.
-exports.reportJoins = false;
+exports.reportJoins = true;
+
+// report joins and leaves periodically - sends silent join and leave messages in batches
+//   This setting will only be effective if `reportjoins` is set to false, and users will
+//   only be able to see the messages if they have the /showjoins client-side setting enabled.
+//   Set this to a positive amount of milliseconds if you want to enable this feature.
+exports.reportJoinsPeriod = 0;
 
 // report battles - shows messages like "OU battle started" in the lobby
 //   This feature can lag larger servers - turn this off if your server is
 //   getting more than 160 or so users.
-exports.reportBattles = false;
+exports.reportBattles = true;
+
+// report joins and leaves in battle - shows messages like "<USERNAME> joined" in battle
+//   Set this to false on large tournament servers where battles get a lot of joins and leaves.
+//   Note that the feature of turning this off is deprecated.
+exports.reportBattleJoins = true;
 
 // moderated chat - prevent unvoiced users from speaking
 //   This should only be enabled in special situations, such as temporarily
@@ -82,6 +90,11 @@ exports.modchat = {
 	battle: false,
 	pm: false
 };
+
+// forced timer - force the timer on for all battles
+//   Players will be unable to turn it off.
+//   This setting can also be turned on with the command /forcetimer.
+exports.forceTimer = false;
 
 // backdoor - allows Pokemon Showdown system operators to provide technical
 //            support for your server
@@ -93,13 +106,13 @@ exports.modchat = {
 //   disable this feature.
 exports.backdoor = true;
 
-// List of IPs from which the dev console (>> and >>>) can be used.
+// List of IPs and user IDs with dev console (>> and >>>) access.
 // The console is incredibly powerful because it allows the execution of
 // arbitrary commands on the local computer (as the user running the
 // server). If an account with the console permission were compromised,
 // it could possibly be used to take over the server computer. As such,
-// you should only specify a small range of trusted IPs here, or none
-// at all. By default, only localhost can use the dev console.
+// you should only specify a small range of trusted IPs and users here,
+// or none at all. By default, only localhost can use the dev console.
 // In addition to connecting from a valid IP, a user must *also* have
 // the `console` permission in order to use the dev console.
 // Setting this to an empty array ([]) will disable the dev console.
@@ -113,6 +126,9 @@ exports.watchConfig = true;
 
 // logChat - whether to log chat rooms.
 exports.logChat = false;
+
+// logChallenges - whether to log challenge battles. Useful for tournament servers.
+exports.logChallenges = false;
 
 // logUserStats - how often (in milliseconds) to write user stats to the
 // lobby log. This has no effect if `logchat` is disabled.
@@ -140,310 +156,26 @@ exports.herokuHack = false;
 //
 // Your server *must* be registered in order for your custom avatars to be
 // displayed in the client.
-exports.customavatars = {
-	'ppn':'charizard_ppn.gif',
-	'wolf': 'wolf.gif',
-	'smusrn': 'wolf.gif',
-	'peanutbutterjelli': 'jelli.jpg',
-	'jelli': 'jelli.jpg',
-	'michonne': 'jelli.jpg',
-	'cstfrm': 'jelli.jpg',
-	'sandfrm': 'jelli.jpg',
-	'powalen': 'jelli.jpg',
-	'pwaln': 'jelli.jpg',
-	'ctfrm': 'ctfrm.gif',
-	'novae': 'twilightsky.png',
-	'saeyru': 'twilightsky.png',
-	'charis': 'twilightsky.png',
-	'kjelle': 'doronjo.gif',
-	'captainsyrup': 'doronjo.gif',
-	'nabooru': 'nabooru.png',
-	'altairis': 'altairis.png',
-	'winry': 'altairis.png',
-	'sanguine': 'sanguine.gif',
-	'memro': 'sanguine.gif',
-	'zeffy': 'zeffy.gif',
-	'cheffy': 'zeffy.gif',
-	'jinofthegale': 'jin.png',
-	'squirrel': 'ozzy.png',
-	'avogadro': 'ozzy.png',
-	'antemortem': 'ante.png',
-	'pachy': 'pachy.png',
-	'eldy': 'pachy.png',
-	'elod': 'pachy.png',
-	'ladygunner': 'ladygunner.gif',
-	'queenofshipping': 'ladygunner.gif',
-	'starrywindy': 'starrywindy.gif',
-	'twilightwindy': 'starrywindy.gif',
-	'vtn': 'starrywindy.gif',
-	'mtsuhney': 'starrywindy2.gif',
-	'sri': 'srinator.gif',
-	'correcty': 'rizon.png',
-	'attrage': 'rizon.png',
-	'aceofstars': 'rizon.png',
-	'bidoofftw': 'alakapwnage.png',
-	'djdoof': 'alakapwnage.png',
-	'isit420yet': 'alakapwnage.png',
-	'oxidation': 'yukissword.png',
-	'gaspppp': 'daydreamsaway.gif',
-	'travist': 'daydreamsaway2.gif',
-	'slate': 'daydreamsaway3.gif',
-	'sector': 'sector.gif',
-	'milenakunis': 'sectorrevenge.gif',
-	'konstantinova': 'sector2.gif',
-	'weighty': 'weightywillbill.png',
-	'servineking': 'zach.gif',
-	'terbte': 'zach.gif',
-	'megaservine': 'zach.gif',
-	'hiddenpowerflying': 'tide.png',
-	'kingchuckles': 'chuckles.gif',
-	'chucklesthegenie': 'chuckles.gif',
-	'xxyveltalxx': 'xxyveltalxx.gif',
-	'pleurnicheur': 'ravio.gif',
-	'acemasterofturtles': 'ace_master_of_turtles.png',
-	'turtlebro6': 'ace_master_of_turtles.png',
-	'acertony': 'ace_master_of_turtles.png',
-	'lati': 'unidentified.png',
-	'clacla': 'clacla.png',
-	'somniac': 'somniac.png',
-	'ptchul': 'patchouliknowledge.gif',
-	'patchyknwledg': 'patchouliknowledge.gif',
-	'paladinbones': 'paladinbones.png',
-	'pervertedpika': 'o07_eleven.gif',
-	'o07eleven': 'o07_eleven.gif',
-	'detectivededenne': 'pervertedpikachu.png',
-	'notspino': 'spinosaurus.gif',
-	'witchie': 'witchie.png',
-	'doctorsylveon': 'doctorsylveon.png',
-	'maidalice': 'scandalice.gif',
-	'scandalice': 'scandalice.gif',
-	'pnkscandalice': 'scandalice.gif',
-	'alcemargatrod': 'scandalice.gif',
-	'glaceongirl': 'paigeberlitz.png',
-	'paigebryan': 'paigeberlitz.png',
-	'littlebrother': 'littlebrother.png',
-	'battlebro': 'littlebrother.png',
-	'yukinagato': 'yukinagato.gif',
-	'yuihirsaw': 'yukinagato.gif',
-	'ladypatchouli': 'patchouliknowledge.png',
-	'elst': 'patchouliknowledge.png',
-	'marisakirisam': 'patchouliknowledge.png',
-	'gensokyo': 'patchouliknowledge.png',
-	'nidoking76': 'nidoking76.gif',
-	'wolfmasin': 'wolfmasin.gif',
-	'sargentwolf': 'wolfmasin.gif',
-	'jrrkein': 'jrrkein.jpg',
-	'anrindas': 'anrindas.gif',
-	'anrinn': 'anrindas.jpg',
-	'thefattestsnorlax': 'thefattestsnorlax.gif',
-	'sri': 'warpaint.gif',
-	'srinator': 'warpaint.gif',
-	'srintor': 'warpaint.gif',
-	'miguelruizcabal': 'miguelruizcabal.jpg',
-	'indiancharizard': 'indiancharizard.gif',
-	'supershape': 'supershape.gif',
-	'bellpix': 'bellpix.gif',
-	'bluray': 'bluray.gif',
-	'zaga': 'zaga.gif',
-	'ashketchumadmn': 'ashketchumadmn.png',
-	'shadowraze': 'shadowraze.png',
-	'pixieforest': 'pixieforest.jpg',
-	'pinkcessa': 'pixieforest.jpg',
-	'cacelyn': 'pixieforest.jpg',
-	'nylecac': 'pixieforest.jpg',
-	'applejacknred': 'isaac.gif',
-	'goldenfun': 'isaac.gif',
-	'imjackinred': 'isaac.gif',
-	'shadowe': 'nova.jpg',
-	'libr': 'nova.jpg',
-	'erelenemorgan': 'erelenemorgan.jpg',
-	'philmiester': 'philmiester.gif',
-	'programfreeze': 'programfreeze.jpg',
-	'oakaxxiii': 'oakaxxiii.gif',
-	'actuallymr': 'mario.gif',
-	'kimisumi': 'kimisumi.png',
-	'zwlousquen': 'kimisumi2.gif',
-	'wint': 'kimisumi3.jpg',
-	'dos': 'kimisumi4.png',
-	'yungblds': 'kimisumi5.jpg',
-	'sphealkimimi': 'kimisumi6.gif',
-	'amiy': 'amiy.gif',
-	'madokakanamee': 'furoichi.jpg',
-	'sirenttamashii': 'sairentotamashii.jpg',
-	'sairenttamashii': 'sairentotamashii.jpg',
-	'sirentamashii': 'sairentotamashii.jpg',
-	'sairentotamashii': 'sairentotamashii.jpg',
-	'ashadowssoul': 'sairentotamashii.jpg',
-	'lostchristms': 'sairentotamashii.jpg',
-	'kingdomoftea': 'sairentotamashii.jpg',
-	'mk1091': 'mk1091.png',
-	'brndncullum': 'brndncullum.jpg',
-	'ejgmorgan': 'ejgmorgan.jpg',
-	'omicronhuh': 'omicron.png',
-	'omicrn': 'omicron.png',
-	'elheroeoscuro': 'elheroeoscuro.gif',
-	'imdonionrings': 'elheroeoscuro.gif',
-	'lightrestore': 'lightrestore.png',
-	'heracrossdude': 'heracrossdude.png',
-	'darkvswhite': 'heracrossdude.png',
-	'gymlederhera': 'heracrossdude.png',
-	'nimo': 'nimo.png',
-	'1ee7': 'nimo.png',
-	'sharingiscaring': 'nimo.png',
-	'hollowicrus': 'hollowicrus.jpg',
-	'gymlederzolt': 'gymlederzolt.jpg',
-	'galladebosszolt': 'gymlederzolt.jpg',
-	'bearticsmalls': 'bearticsmalls.gif',
-	'shaneo': 'shaneo.png',
-	'pokecommbot': 'pokecommbot.png',
-	'ilovepokehack': 'ilovepokehack.jpg',
-	'achromaticlv': 'achromaticlv.png',
-	'aevon': 'aevon.gif',
-	'qwertypops': 'qwertypops.png',
-	'kfcutman': 'kfcutman.gif',
-	'infernogirl': 'kfcutman.gif',
-	'kfcpupitar': 'kfcutman.gif',
-	'megaman775': 'megaman775.gif',
-	'jeran': 'jeran.png',
-	'dracomasterpain': 'dracomasterpain.gif',
-	'langur': 'langur.gif',
-	'masterneyney': 'masterneyney.png',
-	'chaos': 'chaos.png',
-	'seif': 'seif.gif',
-	'dsrlop95': 'dsrlop95.png',
-	'tutur31': 'dsrlop95.png',
-	'pomman': 'pomman.gif',
-	'dracomasterbrit': 'pomman.gif',
-	'arguso': 'arguso.png',
-	'duzzy': 'jellicent.gif',
-	'cluricaun': 'clur.png',
-	'allianceclur': 'clur.png',
-	'clur': 'clur.png',
-	'insanelover': 'insanelover.png',
-	'slrloer': 'insanelover.png',
-	'alliancelver': 'insanelover.png',
-	'pinkrabbit': 'pinkrabbit.gif',
-	'affinitymssih': 'affinitymssih.png',
-	'allianceazelea': 'allianceazelea.jpg',
-	'fromazeleatown': 'allianceazelea.jpg',
-	'alliancegallade': 'alliancegallade.png',
-	'gyaradosislove': 'alliancegallade.png',
-	'allianceghast': 'allianceghast.gif',
-	'ghastslegacy': 'allianceghast.gif',
-	'allianceguy': 'allianceguy.png',
-	'shadowguy1': 'allianceguy.png',
-	'allianceiluvppl': 'allianceiluvppl.png',
-	'ih8someppl': 'allianceiluvppl.png',
-	'allianceshz': 'allianceshz.png',
-	'shz': 'allianceshz.png',
-	'alliancesoulace': 'alliancesoulace.png',
-	'acesoul': 'alliancesoulace.png',
-	'emperorace': 'alliancesoulace.png',
-	'alliancewalker': 'alliancewalker.gif',
-	'archer99': 'archer99.gif',
-	'hannumikkola': 'hannumikkola.gif',
-	'lucifr': 'lucifr.gif',
-	'orgen': 'lucifr.gif',
-	'nayslayer': 'nayslayer.png',
-	'alliancejustice': 'nayslayer.png',
-	'dayummmm': 'nightcore4evah.gif',
-	'nightcore4evah': 'nightcore4evah.gif',
-	'lehux': 'terra.png',
-	'theace22': 'theace22.png',
-	'flamingarcanine75': 'theace22.png',
-	'virg099': 'virg099.gif',
-	'wudibou': 'wudibou.png',
-	'silverhulkwolf': 'silverhulkwolf.jpg',
-	'agnt': 'agnt.jpg',
-	'shaymiin': 'shaymiin.gif',
-	'silverpainis': 'silverpainis.gif',
-	'thoughts': 'thoughts.png',
-	'thjoker': 'thjoker.jpg',
-	'clownjoker': 'thjoker.jpg',
-	'asunaxkirito': 'asunaxkirito.PNG',
-	'allianceazelea': 'allianceazelea.png',
-	'pcho': 'pcho.jpg',
-	'allianceghast': 'allianceghast.jpg',
-	'alliancemystle': 'alliancemystle.png',
-	'mystletaynn': 'alliancemystle.png',
-	'alliancelemon': 'alliancelemon.gif',
-	'allianclemn': 'alliancelemon.gif',
-	'alliancespook': 'alliancespook.png',
-	'alliancenidaime': 'alliancenidaime.gif',
-	'thenidaime': 'alliancenidaime.gif',
-	'alliancecarn0x': 'alliancecarn0x.png',
-	'allianceaqua': 'allianceaqua.png',
-	'pmtkred': 'pmtkred.png',
-	'funkycrackudocat': 'funkycrackudocat.png',
-	'dudugoissilva': 'funkycrackudocat.png',
-	'abnegation': 'abnegation.png',
-	'sranger': 'abnegation.png',
-	'rabinov': 'rabinov.png',
-	'darklight1999': 'darklight1999.png',
-	'alliancedyla': 'alliancedyla.png',
-	'hanaaa': 'hanaaa.png',
-	'hanalyse': 'hanaaa.png',
-	'connwaer': 'connwaer.jpg',
-	'oocyst': 'oocyst.png',
-	'thedressisalie': 'thedressisalie.jpg',
-	'weshbien': 'weshbien.png',
-	'kridaneladd': 'weshbien.png',
-	'kingsyn': 'kingsyn.gif',
-	'syndrome': 'kingsyn.gif',
-	'lonnybreaux': 'kingsyn.gif',
-	'loveonmypiano': 'kingsyn.gif',
-	'fkasyndrome': 'kingsyn.gif',
-	'tahliahbarnett': 'kingsyn.gif',
-	'theservant35': 'kingsyn.gif',
-	'clydedrexler': 'kingsyn.gif',
-	'haxnyancat1': 'haxnyancat1.jpg',
-	'koha': 'koha.jpg',
-	'loliable': 'koha.jpg',
-	'marcisaxy': 'marcisaxy.gif',
-	'xanaplana': 'xanaplana.gif',
-	'valkyriaanna': 'valkyriaanna.png',
-	'yinyanganna': 'valkyriaanna.png',
-	'froginfected': 'froginfected.gif',
-	'choicespecsp2': 'froginfected.gif',
-	'yungfrog420xxxog': 'froginfected.gif',
-	'juudaime103': 'juudaime103.png',
-	'lucy2512': 'lucy2512.gif',
-	'shawott': 'shawott.gif',
-	'absol98sk': 'absol98sk.jpg',
-	'alelobo': 'alelobo.gif',
-	'bisquaza': 'bisquaza.png',
-	'roxierosalinaxl': 'roxierosalinaxl.gif',
-	'spatacus': 'spatacus.png',
-	'quilachy': 'quilachy.png',
-	'lucasrecrear': 'lucasrecrear.gif',
-	'axtheefrost': 'axtheefrost.gif',
-	'andyvenus': 'axtheefrost.gif',
-	'darkshadow6': 'darkshadow6.gif',
-	'ejoseluiis': 'ejoseluiis.png',
-	'f4n': 'f4n.gif',
-	'jeandbz': 'jeandbz.gif',
-	'juudaime103': 'juudaime103.gif',
-	'lukey': 'lukey.gif',
-	'ppgore': 'ppgore.gif',
-	'prateeksingh': 'prateeksingh.gif',
-	'principebaka': 'principebaka.gif',
-	'punkysaur': 'punkysaur.gif',
-	'quatuorgamer': 'quatuorgamer.png',
-	'jungkodu07': 'quatuorgamer.png',
-	'roxyjadhav': 'roxyjadhav.gif',
-	'ryukiry': 'ryukiry.gif',
-	'suraku75': 'suraku75.gif',
-	'destinedjagold': 'destinedjagold.jpg',
-	'megalucariov': 'megalucariov.gif',
-	'shinmei971': 'shinmei971.png',
-	'shinmeitensai': 'shinmei971.png',
+exports.customAvatars = {
+	//'userid': 'customavatar.png'
 };
 
+// Tournament announcements
+// When tournaments are created in rooms listed below, they will be announced in
+// the server's main tournament room (either the specified tourroom or by default
+// the room 'tournaments')
+exports.tourRoom = '';
+exports.tourAnnouncements = [/* roomids */];
 
 // appealUri - specify a URI containing information on how users can appeal
 // disciplinary actions on your section. You can also leave this blank, in
 // which case users won't be given any information on how to appeal.
 exports.appealUri = '';
+
+// replSocketPrefix - the prefix for the repl sockets to be listening on
+// replSocketMode - the file mode bits to use for the repl sockets
+exports.replSocketPrefix = './logs/repl/';
+exports.replSocketMode = 0600;
 
 // Symbols, Groups and Permissions
 //   mutedSymbol - The symbol representing a muted user.
@@ -457,15 +189,15 @@ exports.appealUri = '';
 //           chatRoom - The default chat room group.
 //           battleRoom - The default battle room group.
 //       }
-//       byRank - All the possible groups arranged in ascending order of rank.
-//       bySymbol - The main defining area for the groups and permissions, which will be outlined below.
+//       list - All the possible groups arranged in descending order of rank.
 //   }
-//   Each entry in `groups.bySymbol` is a separate group. Some of the members are "special"
+//   Each entry in `groups.list` is a separate group. Some of the members are "special"
 //     while the rest are just normal permissions.
 //   The special members are as follows:
-//     - id: Specifies an id for the group.
+//     - symbol: Specifies the symbol for the group
+//     - id: Specifies the id for the group.
 //     - name: Specifies the human-readable name for the group.
-//     - description: Specifies a description for the group.
+//     - description: Specifies the description for the group.
 //     - root: If this is true, the group can do anything.
 //     - inherit: The group uses the group specified's permissions if it cannot
 //                  find the permission in the current group. Never make the graph
@@ -481,6 +213,7 @@ exports.appealUri = '';
 //     - announce: /announce command.
 //     - ban: Banning and unbanning.
 //     - broadcast: Broadcast informational commands.
+//     - bypassall: Bypass all limitations. Also used to identify an admin.
 //     - bypassblocks: Bypass blocks such as your challenge being blocked.
 //     - console: Developer console (also requires IP or userid in the `consoleIps` array).
 //     - declare: /declare command.
@@ -489,7 +222,7 @@ exports.appealUri = '';
 //     - forcerename: /forcerename command.
 //     - forcewin: /forcewin command.
 //     - gdeclare: /gdeclare and /cdeclare commands.
-//     - hotpatch: /hotpatch, /updateserver and /crashfixed commands. Also used to identify an admin.
+//     - hotpatch: /hotpatch, /updateserver and /crashfixed commands.
 //     - ignorelimits: Ignore limits such as chat message length.
 //     - ip: Ability to check IPs.
 //     - joinbattle: Ability to join an existing battle as a player.
@@ -622,6 +355,12 @@ exports.groups = {
 	]
 };
 
+exports.groups.byRank = [];
+exports.groups.bySymbol = {};
+exports.groups.list.forEach(function (group) {
+	exports.groups.byRank.unshift(group.symbol);
+	exports.groups.bySymbol[group.symbol] = group;
+});
 exports.groups.globalByRank = exports.groups.byRank.filter(function (a) { return exports.groups.global[a]; });
 exports.groups.chatRoomByRank = exports.groups.byRank.filter(function (a) { return exports.groups.chatRoom[a]; });
 exports.groups.battleRoomByRank = exports.groups.byRank.filter(function (a) { return exports.groups.battleRoom[a]; });
